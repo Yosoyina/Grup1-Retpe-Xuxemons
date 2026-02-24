d2bbd6b526ea1ba65e5ce00ed9b7cc280f181834
Carlos <131690161+Yosoyina@users.noreply.github.com>
2026-02-24 19:08:57 +0100

    Autentificacion parte del Middleware

--- backend/app/Http/Middleware/TuserMiddleware.php
+++ backend/app/Http/Middleware/TuserMiddleware.php
@@ -13,8 +13,17 @@ class TuserMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
+
+    /* En la funcion obtenemos el usuario autenticado usando el guard 'api' y verificamos si su rol es 'tuser'. Si no es así, devolvemos una respuesta JSON con un mensaje de acceso denegado y un código de estado 403.
+    */
+
     public function handle(Request $request, Closure $next): Response
     {
+
+        $user = Auth::guard('api')->user();
+        if (!$user || $user->role !== 'tuser') {
+            return response()->json(['message' => 'Acces denegat'], 403);
+        }
         return $next($request);
     }
 }
