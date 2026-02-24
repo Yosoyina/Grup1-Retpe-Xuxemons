9262651476b75a31b00c3d6c70e6d807c792e7cd
Carlos <131690161+Yosoyina@users.noreply.github.com>
2026-02-24 19:50:47 +0100

    Modificaciones y rutas faltantes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,11 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\UserController;
 
 // ── RUTES PUBLICAS ───────────────────────────────────────
 Route::post('/register',[AuthController::class, 'register']);
