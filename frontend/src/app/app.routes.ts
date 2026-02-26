b7aadb7b43de8b1739894562c576057c7306f9aa
yosoyina <inacio08123@gmail.com>
2026-02-26 23:14:07 +0100

    Nueva Ruta del Menu

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login';
+import { MenuPrincipal } from './menu-principal/menu-principal';
 
 export const routes: Routes = [
   { path: 'registrar', component: RegisterComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'menu', component: MenuPrincipal },
 ];
