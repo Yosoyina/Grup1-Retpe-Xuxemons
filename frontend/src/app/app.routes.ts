58a395b0f1a6c24c963e3535eb7ab9a5f9bc0cb2
yosoyina <inacio08123@gmail.com>
2026-02-26 23:28:35 +0100

    Rutas hijas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,24 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login';
 import { MenuPrincipal } from './menu-principal/menu-principal';
+import { PaginaUsuario } from './pagina-usuario/pagina-usuario';
 
 export const routes: Routes = [
-  { path: 'registrar', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'menu', component: MenuPrincipal },
+  { path: 'registrar', 
+    title: 'Registrar', 
+    component: RegisterComponent 
+  },
+
+  { path: 'login', 
+    title: 'Login'
+    ,component: LoginComponent 
+  },
+
+  { path: 'menu-principal', 
+    title: 'Menu Principal',
+    component: MenuPrincipal, 
+    children: [
+      { path: 'pagina-usuario', title: 'Página de Usuario', component: PaginaUsuario },
+    ]
+  },
 ];
