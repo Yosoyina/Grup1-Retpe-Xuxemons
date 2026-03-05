b4f2ab1d77a8177877e58fd015e3a5338ece7b31
Carlos <131690161+Yosoyina@users.noreply.github.com>
2026-03-05 17:07:14 +0100

    Creacion de Tabla y Campos
    En este aparatado añadimos los campos de la tabla de xuxemons con sus respectivos campos

--- backend/app/Models/Xuxemons.php
+++ backend/app/Models/Xuxemons.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Xuxemons extends Model
 {
-    //
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
+
+    protected $table = 'xuxemons';
+
+    protected $fillable = [
+        'nombre_xuxemon',
+        'tipo_elemento',
+        'grandeza',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'nombre_xuxemon' => 'string',
+            'tipo_elemento' => 'string',
+            'grandeza' => 'string',
+        ];
+    }
 }
