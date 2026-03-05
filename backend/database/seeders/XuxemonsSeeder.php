0a376d0f9a6b67c33559756a44ae5582f3c73da9
yosoyina <inacio08123@gmail.com>
2026-03-05 23:17:41 +0100

    Xuxemons Predeteminados del Seeder

--- backend/database/seeders/XuxemonsSeeder.php
+++ backend/database/seeders/XuxemonsSeeder.php
@@ -15,6 +15,80 @@ class XuxemonsSeeder extends Seeder
      */   
     public function run(): void
     {
-        Xuxemons::factory()->count(10)->create();
+        $xuxemons = [
+
+            // Aigua
+            
+            [
+             'nom' => 'Goteta',
+             'tipus' => 'Aigua',
+             'mida' => 'Petit',
+             'descripcio' => "Una petita gota d'aigua que salta alegrement.",
+            ],
+            [
+             'nom' => 'Bassot',
+             'tipus' => 'Aigua',
+             'mida' => 'Mitjà',
+             'descripcio' => 'Un bassiot profund ple de misteris aquàtics.',
+            ],
+            [
+             'nom' => 'Laguna',
+             'tipus' => 'Aigua',
+             'mida' => 'Gran',
+             'descripcio' => "Una laguna ancestral plena d'energia hidden.",
+            ],
+
+            // Aire
+
+            [
+             'nom' => 'Sospir',
+             'tipus' => 'Aire',
+             'mida' => 'Petit',
+             'descripcio' => 'Un sospir de vent calent que escampa les llavors.',
+            ],
+            [
+             'nom' => 'Ratxot',
+             'tipus' => 'Aire',
+             'mida' => 'Mitjà',
+             'descripcio' => 'Una ratxa de vent que apareix de forma inesperada.',
+            ],
+            [
+             'nom' => 'Estratós',
+             'tipus' => 'Aire',
+             'mida' => 'Gran',
+             'descripcio' => "Un ésser de l'estratosfera amb poder il·limitat.",
+            ],
+
+            // Terra
+
+            [
+             'nom' => 'Fanguet',
+             'tipus' => 'Terra',
+             'mida' => 'Petit',
+             'descripcio' => "Un fanguet tou que s'infiltra per qualsevol escletxa.",
+            ],
+            [
+             'nom' => 'Escarpat',
+             'tipus' => 'Terra',
+             'mida' => 'Mitjà',
+             'descripcio' => 'Un escarpat rocós que escala qualsevol superfície.',
+            ],
+            [
+             'nom' => 'Terramut',
+             'tipus' => 'Terra',
+             'mida' => 'Gran',
+             'descripcio' => 'Un colossal guardià de terra que fa trontollar el sòl.',
+            ],
+            [
+             'nom' => 'Megalit',
+             'tipus' => 'Terra',
+             'mida' => 'Gran',
+             'descripcio' => 'Un megalit antic amb poder ancestral immens.',
+            ],
+        ];
+
+        foreach ($xuxemons as $data) {
+            Xuxemons::create($data);
+        }
     }
 }
