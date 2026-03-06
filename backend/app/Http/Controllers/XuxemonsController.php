bea1a33fb4966e577e8dc492cd1eecf77c272035
yosoyina <inacio08123@gmail.com>
2026-03-06 01:24:32 +0100

    Listado de la Xuxeded de los Xuxemons

--- backend/app/Http/Controllers/XuxemonsController.php
+++ backend/app/Http/Controllers/XuxemonsController.php
@@ -4,23 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Xuxemons;
+use Illuminate\Support\Facades\DB;
 
 class XuxemonsController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    // ── XUXEDEX ─────────────────────────────────────────────
+
+    public function index(Request $request)
     {
-        //
+        $tipo   = $request->tipo_elemento;
+        $tamano = $request->tamano;
+
+        $xuxemons = DB::select("
+            SELECT *
+            FROM xuxemons
+            WHERE (tipo_elemento = :tipo   OR :tipo   IS NULL)
+            AND (tamano = :tamano OR :tamano IS NULL)
+        ", [
+            'tipo'   => $tipo,
+            'tamano' => $tamano,
+        ]);
+
+        return response()->json($xuxemons, 200);
     }
 
     /**
