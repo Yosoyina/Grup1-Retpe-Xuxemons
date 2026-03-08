4f578eb7928ae9f3c9fe85e7debb60fe605e5487
yosoyina <inacio08123@gmail.com>
2026-03-08 21:00:00 +0100

    Eliminacion de Metodos del Crud

--- backend/app/Http/Controllers/XuxemonsController.php
+++ backend/app/Http/Controllers/XuxemonsController.php
@@ -119,22 +119,6 @@ class XuxemonsController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, string $id)
