92c40d748305b263c1c43b1f11fd38837d107ab7
yosoyina <inacio08123@gmail.com>
2026-03-09 22:33:35 +0100

    Migracion de la Tabla Xuxes

---
+++ backend/database/migrations/2026_03_09_213153_create_xuxes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('xuxes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_xuxes');
+            $table->string('descripcio')->nullable();
+            $table->string('imagen')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('xuxes');
+    }
+};
