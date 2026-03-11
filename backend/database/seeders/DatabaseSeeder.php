7a6c4678c544489fce32935efb596588aa511e2c
Carlos <131690161+Yosoyina@users.noreply.github.com>
2026-03-11 17:02:09 +0100

    Descomentado el Seeder de Inventario

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -20,7 +20,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([UserSeeder::class]);
-        // $this->call([InventarioSeeder::class]);
+        $this->call([InventarioSeeder::class]);
         $this->call([XuxemonsSeeder::class]);
     }
 }
