a4afeab35112477b6c6b1901571021903133baa6
yosoyina <inacio08123@gmail.com>
2026-03-12 00:43:13 +0100

    Rutas Api de Xuxemosn e Inventario
    Tanto las dos rutas de inventario y xuxedex devuelve todo lo que tiene en el inventrio el jugador y los xuxemons en general y despues  deuvuelven por la id un xuxemon concreto o el inevntario estas rutas son de lectura para el usuario como las modificaciones que puede hacer en su perfil

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\XuxemonsController;
+use App\Http\Controllers\InventarioController;
 
 // ── RUTES PUBLIQUES ───────────────────────────────────────
 Route::post('/register', [AuthController::class, 'register']);
@@ -21,5 +21,15 @@ Route::middleware('auth:api')->group(function () {
 
     // ── XUXEDEX ROUTES ───────────────────────────────────
     Route::get('/xuxedex', [XuxemonsController::class, 'getUserXuxedex']);
-    Route::apiResource('/xuxemons', XuxemonsController::class);
+    //Route::apiResource('/xuxemons', XuxemonsController::class);
+
+    // ── POKEDEX DE XUXEMONS ───────────────────────────────────
+    Route::get('/xuxemons',      [XuxemonsController::class, 'index']);
+    Route::get('/xuxemons/{id}', [XuxemonsController::class, 'show']);
+
+    // ── iNVENTARIO DEL JUGADOR ───────────────────────────────────
+
+    Route::get('/inventario',      [InventarioController::class, 'index']);
+    Route::get('/inventario/{id}', [InventarioController::class, 'show']);
+
 });
