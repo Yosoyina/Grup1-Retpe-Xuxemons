866d758bb5df6ebcd6fdde52b2874f16ec5989e7
Marc Cunillera <marccunillera2006@gmail.com>
2026-05-05 16:48:03 +0200

    usuariuo admin

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
 use App\Services\XuxedexService;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -15,6 +16,15 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         $xuxedexService = app(XuxedexService::class);
+
+        $admin = User::create([
+            'nombre'    => 'admin',
+            'apellidos' => 'admin',
+            'email'     => 'admin@gmail.com',
+            'password'  => Hash::make('password'),
+            'role'      => 'admin',
+        ]);
+        $xuxedexService->ensureStarterXuxedex($admin->id);
 
         User::factory()->count(10)->create()
             ->each(fn (User $user) => $xuxedexService->ensureStarterXuxedex($user->id));
